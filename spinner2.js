f132bc206d6911957740e2556934da1771dfdf6d
Daniel Soares <danielsoares01@outlook.com>
2023-02-26 17:09:56 -0800

    spinner2 function

---
+++ spinner2.js
@@ -0,0 +1,10 @@
+const chars = ["|", "/", "-", "\\", "|", "/", "-", "\\"];
+
+let when = 0;
+for (const char of chars) {
+  when += 200;
+  // console.log(char, when);
+  setTimeout(() => {
+    process.stdout.write(`\r${char}   `);
+  }, when);
+}
